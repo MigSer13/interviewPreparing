67187356979cc90fa39813d0d2d143ccaf02349d
MigSer13 <avto470@mail.ru>
2021-12-09 01:11:57 +0300

    lesson 7+

--- src/main/java/ru/home/lesson7/StudentController.java
+++ src/main/java/ru/home/lesson7/StudentController.java
@@ -3,11 +3,9 @@ package ru.home.lesson7;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class StudentController {
     private StudentRepository studentRepository;
 
